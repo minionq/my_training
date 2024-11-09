18b6506a7032ab4a0273d586089c3e7cbcb74571
Арина <tochnonearina@gmail.com>
2024-11-10 01:03:19 +0300

    new

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#1st program
+#1 program
 print((9**0.5)*5)
-#2nd program
+#2 program
 print((9.99 > 9.98) and (1000 != 1000.1))
-#3rd program
+#3 program
 print(2*2+2)
 print(2*(2+2))
 print(2*2+2 == 2*(2+2))
-#4th program
+#4 program
 print(int(float('123.456')*10)%10)
